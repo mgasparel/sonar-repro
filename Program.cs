f3c2de9153685a25e0261684c0e115ff2f32020c
Michael Gasparelli <michaelgasparelli@geotab.com>
2020-09-23 08:42:51 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sonar_repro
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ S1128.cs
@@ -0,0 +1,10 @@
+using System; // Compliant, used by cref.
+
+namespace SonarAnalyzer.Experiments.CSharp
+{
+    public enum S1128
+    {
+        /// <summary><see cref="DateTime"/></summary>
+        DateTimeValue,
+    }
+}
